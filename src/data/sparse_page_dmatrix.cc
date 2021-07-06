93b0c0d33b1120c33960b8d46e0a87273e9c824e
fis <jm.yuan@outlook.com>
2021-07-06 16:47:08 +0800

    allreduce.

--- src/data/sparse_page_dmatrix.cc
+++ src/data/sparse_page_dmatrix.cc
@@ -54,6 +54,8 @@ SparsePageDMatrix::SparsePageDMatrix(DataIterHandle iter_handle, DMatrixHandle p
   this->info_.num_col_ = n_features;
   CHECK_NE(n_features, 0);
   this->info_.num_nonzero_ = nnz;
+
+  rabit::Allreduce<rabit::op::Max>(&info_.num_col_, 1);
 }
 
 void SparsePageDMatrix::InitializeSparsePage() {
