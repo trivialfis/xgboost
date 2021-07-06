8d6867d0022f517f896872fb7d74905a5b79046c
fis <jm.yuan@outlook.com>
2021-07-06 16:44:08 +0800

    Robust test.

--- tests/cpp/data/test_file_iterator.cc
+++ tests/cpp/data/test_file_iterator.cc
@@ -14,17 +14,24 @@
 namespace xgboost {
 namespace data {
 TEST(FileIterator, Basic) {
+  auto check_n_features = [](FileIterator *iter) {
+    size_t n_features = 0;
+    iter->Reset();
+    while (iter->Next()) {
+      auto proxy = MakeProxy(iter->Proxy());
+      auto csr = dmlc::get<std::shared_ptr<CSRArrayAdapter>>(proxy->Adapter());
+      n_features = std::max(n_features, csr->NumColumns());
+    }
+    ASSERT_EQ(n_features, 5);
+  };
+
   dmlc::TemporaryDirectory tmpdir;
   {
     auto zpath = tmpdir.path + "/0-based.svm";
     CreateBigTestData(zpath, 3 * 64, true);
     zpath += "?indexing_mode=0";
     FileIterator iter{zpath, 0, 1, "libsvm"};
-    iter.Reset();
-    iter.Next();
-    auto proxy = MakeProxy(iter.Proxy());
-    auto csr = dmlc::get<std::shared_ptr<CSRArrayAdapter>>(proxy->Adapter());
-    ASSERT_EQ(csr->NumColumns(), 5);
+    check_n_features(&iter);
   }
 
   {
@@ -32,11 +39,7 @@ TEST(FileIterator, Basic) {
     CreateBigTestData(opath, 3 * 64, false);
     opath += "?indexing_mode=1";
     FileIterator iter{opath, 0, 1, "libsvm"};
-    iter.Reset();
-    iter.Next();
-    auto proxy = MakeProxy(iter.Proxy());
-    auto csr = dmlc::get<std::shared_ptr<CSRArrayAdapter>>(proxy->Adapter());
-    ASSERT_EQ(csr->NumColumns(), 5);
+    check_n_features(&iter);
   }
 }
 }  // namespace data
