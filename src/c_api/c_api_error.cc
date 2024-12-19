467b96c0aaf392c73a9f836bc1b36d841ccf799d
Jiaming Yuan <jm.yuan@outlook.com>
2024-12-19 19:27:36 +0800

    jvm cache.

--- src/c_api/c_api_error.cc
+++ src/c_api/c_api_error.cc
@@ -8,8 +8,6 @@
 #include <dmlc/thread_local.h>
 
 #include "xgboost/c_api.h"
-#include "../collective/comm.h"
-#include "../collective/comm_group.h"
 
 struct XGBAPIErrorEntry {
   std::string last_error;
