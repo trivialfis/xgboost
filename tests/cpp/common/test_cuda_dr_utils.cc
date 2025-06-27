b613669befbfce9f8a30be45256f5252d9525953
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-27 15:02:51 +0800

    cross check.

--- tests/cpp/common/test_cuda_dr_utils.cc
+++ tests/cpp/common/test_cuda_dr_utils.cc
@@ -4,6 +4,9 @@
 #include <gtest/gtest.h>
 
 #if defined(XGBOOST_USE_CUDA) && defined(__linux__)
+#include <unordered_set>
+
+#include "../../../src/common/common.h"
 #include "../../../src/common/cuda_dr_utils.h"
 
 namespace xgboost::cudr {
@@ -49,6 +52,55 @@ TEST(DrUtils, GetC2cLinkCountFromSmi) {
   }
   {
     [[maybe_unused]] auto _ = GetC2cLinkCountFromSmiGlobal();
+  }
+}
+
+namespace {
+std::string GetCuDeviceUuid(std::int32_t ordinal) {
+  CUuuid dev_uuid;
+  std::stringstream s;
+  std::unordered_set<unsigned char> dashPos{0, 4, 6, 8, 10};
+  cudr::GetGlobalCuDriverApi().cuDeviceGetUuid(&dev_uuid, ordinal);
+
+  s << "GPU";
+  for (int i = 0; i < 16; i++) {
+    if (dashPos.count(i)) {
+      s << '-';
+    }
+    s << std::hex << std::setfill('0') << std::setw(2)
+      << (0xFF & static_cast<std::int32_t>(dev_uuid.bytes[i]));
+  }
+  return s.str();
+}
+
+std::string GetCudaUUID(std::int32_t ordinal) {
+  cudaDeviceProp prob{};
+  dh::safe_cuda(cudaGetDeviceProperties(&prob, ordinal));
+
+  std::unordered_set<unsigned char> dashPos{0, 4, 6, 8, 10};
+  std::stringstream s;
+  s << "GPU";
+  for (int i = 0; i < 16; i++) {
+    if (dashPos.count(i)) {
+      s << '-';
+    }
+    s << std::hex << std::setfill('0') << std::setw(2)
+      << (0xFF & static_cast<std::int32_t>(prob.uuid.bytes[i]));
+  }
+
+  return s.str();
+}
+}  // namespace
+
+TEST(DrUtils, GetUUID) {
+  [[maybe_unused]] auto& _ = cudr::GetGlobalCuDriverApi();
+  std::cout << "Driver" << std::endl;
+  for (std::size_t i = 0; i < 2; ++i) {
+    std::cout << "i:" << i << " " << GetCuDeviceUuid(i) << std::endl;
+  }
+  std::cout << "Runtime" << std::endl;
+  for (std::size_t i = 0; i < 2; ++i) {
+    std::cout << "i:" << i << " " << GetCudaUUID(i) << std::endl;
   }
 }
 }  // namespace xgboost::cudr
