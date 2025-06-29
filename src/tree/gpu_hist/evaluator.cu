1106905937ca981e4ff9d116aa8442ada0ee74ea
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-29 15:53:20 +0800

    revert.

--- src/tree/gpu_hist/evaluator.cu
+++ src/tree/gpu_hist/evaluator.cu
@@ -83,10 +83,10 @@ common::Span<bst_feature_t const> GPUHistEvaluator::SortHistogram(
                       if (common::IsCat(shared_inputs.feature_types, fidx)) {
                         auto grad =
                             shared_inputs.rounding.ToFloatingPoint(input.gradient_histogram[j]);
-                        auto lw = evaluator.CalcWeightCat(shared_inputs.param, grad);
+                        float lw = evaluator.CalcWeightCat(shared_inputs.param, grad);
                         return thrust::make_tuple(i, lw);
                       }
-                      return thrust::make_tuple(i, 0.0);
+                      return thrust::make_tuple(i, 0.0f);
                     });
   // Sort an array segmented according to
   // - nodes
