ad2db37cb0d6b7f9a1c252beafcb1cca4ca06e39
Jiaming Yuan <jm.yuan@outlook.com>
2025-06-30 14:21:44 +0800

    Print.

--- src/tree/gpu_hist/evaluate_splits.cu
+++ src/tree/gpu_hist/evaluate_splits.cu
@@ -123,10 +123,10 @@ class EvaluateSplitAgent {
       double gain = thread_active ? LossChangeMissing(bin, missing, parent_sum, param, nidx, fidx,
                                                       evaluator, missing_left, rounding)
                                   : kNullGain;
-      if (blockIdx.x == 0) {
-        printf("t: %d, gain: %f, g: %" PRId64 ", h: %" PRId64 "\n", int(threadIdx.x), gain,
-               bin.GetQuantisedGrad(), bin.GetQuantisedHess());
-      }
+      // if (blockIdx.x == 0) {
+      //   printf("t: %d, gain: %f, g: %" PRId64 ", h: %" PRId64 "\n", int(threadIdx.x), gain,
+      //          bin.GetQuantisedGrad(), bin.GetQuantisedHess());
+      // }
       // Find thread with best gain
       auto best = MaxReduceT(temp_storage->max_reduce).Reduce({threadIdx.x, gain}, cub::ArgMax());
       // This reduce result is only valid in thread 0
@@ -147,6 +147,9 @@ class EvaluateSplitAgent {
         GradientPairInt64 right = parent_sum - left;
         best_split->Update(gain, missing_left ? kLeftDir : kRightDir, fvalue, fidx, left, right,
                            false, param, rounding);
+        if (blockIdx.x == 0) {
+          printf("update gain %f\n", best_split->loss_chg);
+        }
       }
     }
   }
