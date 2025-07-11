d86dbb977e25cf7ac95a4623917fd9b82194441f
Jiaming Yuan <jm.yuan@outlook.com>
2025-07-11 18:36:20 +0800

    work on get mem policy

--- src/common/numa_utils.cc
+++ src/common/numa_utils.cc
@@ -1,4 +1,4 @@
-#include <linux/mempolicy.h>
+#include <linux/mempolicy.h>  // for MPOL_BIND
 #include <sys/syscall.h>
 #include <unistd.h>
 
@@ -10,19 +10,17 @@
 #include <vector>      // for vector
 
 #include "cuda_rt_utils.h"
+#include "error_msg.h"  // for SystemError
+#include "xgboost/logging.h"
 
 namespace xgboost::common {
-void GetNumaNodeCpus(std::vector<std::int32_t>* p_cpus) {
-  namespace fs = std::filesystem;
-  std::int32_t nodeid = curt::GetNumaId();
-  std::string nodename = "node" + std::to_string(nodeid);
-  auto cpulist_p = fs::path{"/sys/devices/system/node"} / fs::path{nodename} / fs::path{"cpulist"};
-  if (!fs::exists(cpulist_p)) {
-    return;
-  }
+namespace {
+namespace fs = std::filesystem;
 
-  auto n_bytes = fs::file_size(cpulist_p);
-  std::ifstream fin{cpulist_p};
+// Read a file with the `cpulist` format.
+void ReadCpuList(fs::path const &path, std::vector<std::int32_t> *p_cpus) {
+  auto n_bytes = fs::file_size(path);
+  std::ifstream fin{path};
   std::string buffer(n_bytes, 0);
   fin.read(buffer.data(), buffer.size());
   auto pos = std::find(buffer.cbegin(), buffer.cend(), '-');
@@ -32,35 +30,57 @@ void GetNumaNodeCpus(std::vector<std::int32_t>* p_cpus) {
   auto start = std::stoi(buffer.substr(0, std::distance(buffer.cbegin(), pos)));
   auto end = std::stoi(buffer.substr(std::distance(buffer.cbegin(), pos) + 1));
 
-  auto& cpus = *p_cpus;
+  auto &cpus = *p_cpus;
   for (auto i = start; i <= end; ++i) {
     cpus.push_back(i);
   }
 }
 
-void SetNumaCpuAffinity(std::vector<std::int32_t> const& cpus) {
-  cpu_set_t cpuset;
-  CPU_ZERO(&cpuset);
+// Wrapper for the system call
+auto GetMemPolicy(int *mode, unsigned long *nodemask, unsigned long maxnode, void *addr,
+                  unsigned long flags) {
+  return syscall(SYS_get_mempolicy, mode, nodemask, maxnode, addr, flags);
+}
+}  // namespace
 
-  for (auto cpu_id : cpus) {
-    CPU_SET(cpu_id, &cpuset);
+void GetNumaNodeCpus(std::vector<std::int32_t> *p_cpus) {
+  std::int32_t nodeid = curt::GetNumaId();
+  std::string nodename = "node" + std::to_string(nodeid);
+  auto cpulist_p = fs::path{"/sys/devices/system/node"} / fs::path{nodename} / fs::path{"cpulist"};
+  if (!fs::exists(cpulist_p)) {
+    return;
   }
-  // fixme: is there a way to set for the entire process?
-  if (sched_setaffinity(0, sizeof(cpuset), &cpuset) == -1) {
-    std::perror("sched_setaffinity");
-    // fixme, error handling.
+  ReadCpuList(cpulist_p, p_cpus);
+}
+
+auto GetMemPolicy(int *policy, unsigned long *nmask, unsigned long maxnode) {
+  return GetMemPolicy(policy, nmask, maxnode, nullptr, 0);
+}
+
+[[nodiscard]] std::size_t GetMaxNumNodes() {
+  auto p_possible = fs::path{"/sys/devices/system/node/possible"};
+  if (!fs::exists(p_possible)) {
+    return 0;
+  }
+
+  std::int32_t max_num_nodes = sizeof(uint64_t) * 8;
+  while (true) {
+    std::vector<std::uint64_t> mask(max_num_nodes);
+
+    std::int32_t mode = -1;
+    auto err = GetMemPolicy(&mode, mask.data(), max_num_nodes);
+    if (!err || errno != EINVAL) {
+      return max_num_nodes;
+    }
+    max_num_nodes *= 2;
   }
 }
 
-void SetNumaMemoryAffinity(std::int32_t node_id) {
-  std::uintmax_t nodemask = 1UL << node_id;
-  int mode = MPOL_BIND;
-  auto maxnode = sizeof(nodemask) * 8;  // fixme: not true
-
-  auto ret = syscall(SYS_set_mempolicy, mode, &nodemask, maxnode);
-  if (ret != 0) {
-    std::perror("set_mempolicy");
-    // fixme: error handling.
-  }
+[[nodiscard]] bool GetNumaMemBind() {
+  std::int32_t mode = -1;
+  auto max_n_nodes = GetMaxNumNodes();
+  std::vector<std::uint64_t> mask(max_n_nodes / 8);
+  CHECK_GE(GetMemPolicy(&mode, mask.data(), max_n_nodes), 0) << error::SystemError().message();
+  return mode == MPOL_BIND;
 }
 }  // namespace xgboost::common
--- src/common/numa_utils.h
+++ src/common/numa_utils.h
@@ -4,4 +4,8 @@
 
 namespace xgboost::common {
 void GetNumaNodeCpus(std::vector<std::int32_t>* p_cpus);
-}
+
+[[nodiscard]] std::size_t GetMaxNumNodes();
+
+[[nodiscard]] bool GetNumaMemBind();
+}  // namespace xgboost::common
