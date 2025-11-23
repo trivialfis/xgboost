ebe65537e5a35670bf7ca14a3ac5a93257f46f47
Jiaming Yuan <jm.yuan@outlook.com>
2025-11-24 01:07:04 +0800

    read only test.

--- tests/cpp/tree/gpu_hist/test_multi_histogram.cu
+++ tests/cpp/tree/gpu_hist/test_multi_histogram.cu
@@ -4,6 +4,7 @@
 #include <gtest/gtest.h>
 #include <thrust/sequence.h>
 
+#include "../../../../src/common/device_debug.cuh"
 #include "../../../../src/tree/gpu_hist/histogram.cuh"
 #include "../../helpers.h"
 #include "../../histogram_helpers.h"
@@ -50,6 +51,40 @@ TEST(GpuMultiHistogram, Basic) {
   // }
 }
 
+namespace {
+XGBOOST_DEV_INLINE bst_feature_t FeatIdx(bst_idx_t idx, std::int32_t feature_stride) {
+  auto fidx = idx % feature_stride;
+  return fidx;
+}
+
+template <typename IterT>
+XGBOOST_DEV_INLINE bst_idx_t IterIdx(EllpackAccessorImpl<IterT> const& matrix, std::uint32_t ridx,
+                                     bst_feature_t fidx) {
+  // ridx_local = ridx - base_rowid  <== Row index local to each batch
+  // entry_idx = ridx_local * row_stride <== Starting entry index for this row in the matrix
+  // entry_idx += start_feature  <== Inside a row, first column inside this feature group
+  // idx % feature_stride <== The feaature index local to the current feature group
+  // entry_idx += idx % feature_stride <== Final index.
+  return (ridx - matrix.base_rowid) * matrix.row_stride + fidx;
+}
+
+__global__ void TestHistBuildKernel(EllpackDeviceAccessor matrix,
+                                    common::Span<GradientPairInt64> d_node_hist,
+                                    common::Span<std::uint32_t> d_ridx,
+                                    common::Span<GradientQuantiser const> roundings) {
+  bst_idx_t n_elements = matrix.row_stride * d_ridx.size();
+  for (auto i : dh::GridStrideRange(static_cast<std::size_t>(0), n_elements)) {
+    std::uint32_t ridx = i / matrix.row_stride;
+    auto fidx = FeatIdx(i, matrix.row_stride);
+    auto idx = IterIdx(matrix, ridx, fidx);
+    bst_bin_t compressed_bin = matrix.gidx_iter[idx];
+    if (compressed_bin == -1) {
+      printf("-1\n");
+    }
+  }
+}
+}  // namespace
+
 TEST(GpuMultiHistogram, Large) {
   auto ctx = MakeCUDACtx(0);
 
@@ -80,15 +115,25 @@ TEST(GpuMultiHistogram, Large) {
   histogram.AllocateHistograms(&ctx, {0});
   auto node_hist = histogram.GetNodeHistogram(0);
 
-  if (use_single_target) {
-    GradientQuantiser q{GradientPairPrecise{1.0f, 1.0f}, GradientPairPrecise{1.0f, 1.0f}};
-    histogram.BuildHistogram(ctx.CUDACtx(), page->GetDeviceEllpack(&ctx, {}), fg_acc,
-                             gpairs.View(ctx.Device()).Values(), dh::ToSpan(ridx), node_hist, q);
-  } else {
-    auto quantizers = MakeDummyQuantizers(n_targets);
-    histogram.BuildHistogram(ctx.CUDACtx(), page->GetDeviceEllpack(&ctx, {}), fg_acc,
-                             gpairs.View(ctx.Device()), dh::ToSpan(ridx), node_hist,
-                             dh::ToSpan(quantizers));
-  }
+  auto quantizers = MakeDummyQuantizers(n_targets);
+  constexpr std::uint32_t kBlockThreads = 512;
+  auto n = page->Size() * page->info.row_stride;
+  auto n_grids = common::DivRoundUp(n, kBlockThreads);
+
+  TestHistBuildKernel<<<n_grids, kBlockThreads>>>(
+      std::get<EllpackDeviceAccessor>(page->GetDeviceEllpack(&ctx, {})), node_hist,
+      dh::ToSpan(ridx), dh::ToSpan(quantizers));
+  debug::SyncDevice();
+
+  // if (use_single_target) {
+  //   GradientQuantiser q{GradientPairPrecise{1.0f, 1.0f}, GradientPairPrecise{1.0f, 1.0f}};
+  //   histogram.BuildHistogram(ctx.CUDACtx(), page->GetDeviceEllpack(&ctx, {}), fg_acc,
+  //                            gpairs.View(ctx.Device()).Values(), dh::ToSpan(ridx), node_hist, q);
+  // } else {
+  //   auto quantizers = MakeDummyQuantizers(n_targets);
+  //   histogram.BuildHistogram(ctx.CUDACtx(), page->GetDeviceEllpack(&ctx, {}), fg_acc,
+  //                            gpairs.View(ctx.Device()), dh::ToSpan(ridx), node_hist,
+  //                            dh::ToSpan(quantizers));
+  // }
 }
 }  // namespace xgboost::tree::cuda_impl
