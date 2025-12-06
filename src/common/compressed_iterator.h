2e2663670e9a126c33bb207192eb637b53cd32b0
Jiaming Yuan <jm.yuan@outlook.com>
2025-12-06 15:34:43 +0800

    Revert "normal prefetch."
    This reverts commit 704e3e588b50a8f1d26996f86d43dc1c9ca7f00c.

--- src/common/compressed_iterator.h
+++ src/common/compressed_iterator.h
@@ -168,8 +168,7 @@ XGBOOST_DEVICE inline std::size_t AlignDown(std::size_t value, std::size_t align
 #if __CUDA_ARCH__ >= 900
 __device__ inline void PrefetchGlobalL2(void const *addr) {
   addr = reinterpret_cast<void const *>(AlignDown(reinterpret_cast<ptrdiff_t>(addr), 16));
-  // asm volatile("cp.async.bulk.prefetch.L2.global [%0], 32;" ::"l"(__cvta_generic_to_global(addr)));
-  asm volatile("prefetch.global.L2 [%0];" : : "l"(__cvta_generic_to_global(addr)) : "memory");
+  asm volatile("cp.async.bulk.prefetch.L2.global [%0], 32;" ::"l"(__cvta_generic_to_global(addr)));
 }
 #else
 template <typename T>
