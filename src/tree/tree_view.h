9934c1b94bde749c8fee42c673b9c3a413bd002a
Jiaming Yuan <jm.yuan@outlook.com>
2026-01-12 19:07:50 +0800

    Avoid copying stats if not needed.

--- src/tree/tree_view.h
+++ src/tree/tree_view.h
@@ -1,5 +1,5 @@
 /**
- * Copyright 2025, XGBoost Contributors
+ * Copyright 2025-2026, XGBoost Contributors
  *
  * The file provides views for two tree models. We hope to eventually unify them, but the
  * original scalar tree `Node` struct is used extensively in the codebase.
@@ -151,7 +151,7 @@ struct ScalarTreeView : public WalkTreeMixIn<ScalarTreeView>, public CategoriesM
       : CategoriesMixIn{std::move(cats)}, nodes{nodes}, stats{stats}, n{n_nodes} {}
 
   /** @brief Create a device view */
-  explicit ScalarTreeView(DeviceOrd device, RegTree const* tree);
+  explicit ScalarTreeView(DeviceOrd device, bool need_stat, RegTree const* tree);
   /** @brief Create a host view */
   explicit ScalarTreeView(RegTree const* tree)
       : CategoriesMixIn{tree->GetCategoriesMatrix(DeviceOrd::CPU())},
